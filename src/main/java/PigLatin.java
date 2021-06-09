7b956a90dfa49a5b1fa43032f50caa5d6605c89f
Angelica <Angelica137@users.noreply.github.com>
2021-06-09 20:20:56 +0100

    word starts with vowel returns 'ay' at end

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -1,5 +1,10 @@
 public class PigLatin {
 	public String translate(String word) {
+		char start = word.charAt(0);
+		char start2 = word.charAt(1);
+		if (start == 'a' || start == 'e' || start == 'i' || start == 'o' || start == 'u') {
+			return (word + "ay");
+		}
 		return word;
 	}
 }
--- src/test/java/PigLatinTest.java
+++ src/test/java/PigLatinTest.java
@@ -1,6 +1,7 @@
-import static org.assertj.core.api.Assertions.assertThat;
+
+//import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
+//import static org.junit.Assert.assertThrows;
 
 import org.junit.Test;
 import org.junit.Before;
@@ -16,5 +17,10 @@ public class PigLatinTest {
 	@Test
 	public void testPigLatinReturnsString() {
 		assertEquals("Hello", pig.translate("Hello"));
+	}
+
+	@Test
+	public void testWordStartsWithVowelEndsAy() {
+		assertEquals("earay", pig.translate("ear"));
 	}
 }
