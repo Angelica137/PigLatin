27bb13649736fa04b86d53c7f2efb0e0df0d0946
Angelica <Angelica137@users.noreply.github.com>
2021-06-09 19:20:43 +0100

    test .translate() returns a tring

--- src/test/java/PigLatinTest.java
+++ src/test/java/PigLatinTest.java
@@ -1,3 +1,20 @@
-public class PigLatinTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
+import org.junit.Test;
+import org.junit.Before;
+
+public class PigLatinTest {
+	private PigLatin pig;
+
+	@Before
+	public void setup() {
+		pig = new PigLatin();
+	}
+
+	@Test
+	public void testPigLatinReturnsString() {
+		assertEquals("Hello", pig.translate("Hello"));
+	}
 }
